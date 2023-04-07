c8a75285b175657f661663d04e2b23309605c756
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-04-07 11:15:43 +0500

    Add files via upload
    first js assgiment

---
+++ assgiment one js/app.js
@@ -0,0 +1,118 @@
+//question no 1
+
+//let num1 = prompt("Enter the first number:");
+//let num2 = prompt("Enter the second number:");
+
+//if (num1 > num2) {
+ // console.log("The larger number is " + num1);
+//} else if (num2 > num1) {
+ // console.log("The larger number is " + num2);
+//} else {
+ // console.log("The numbers are equal");
+//}
+
+// question no 2
+
+//let num = prompt("Enter a number:");
+
+//if (num > 0) {
+  //alert("The sign is +");
+//} else if (num < 0) {
+ // alert("The sign is -");
+//} else {
+ // alert("The number is zero");
+//}
+
+//question no 3
+
+//let num1 = prompt("Enter the first number:");
+//let num2 = prompt("Enter the second number:");
+//let num3 = prompt("Enter the third number:");
+//let num4 = prompt("Enter the fourth number:");
+//let num5 = prompt("Enter the fifth number:");
+
+//let largest = num1; // assume the first number is the largest
+
+//if (num2 > largest) {
+ // largest = num2;
+//}
+
+//if (num3 > largest) {
+//  largest = num3;
+//}
+
+//if (num4 > largest) {
+ // largest = num4;
+//}
+
+//if (num5 > largest) {
+ // largest = num5;
+//}
+
+//console.log("The largest number is " + largest);
+
+//question no 4
+
+//for (let i = 0; i <= 15; i++) {
+  //if (i % 2 === 0) {
+    //console.log(i + " is even");
+  //} else {
+   // console.log(i + " is odd");
+  //}
+//}
+
+//question no 5
+
+//let studentMarks = {
+ // John: 80,
+ // Jane: 90,
+// Bob: 75,
+// Mike: 60,
+// Emily: 85
+//};
+
+//let totalMarks = 0;
+
+//for (let student in studentMarks) {
+ // totalMarks += studentMarks[student];
+//}
+
+//let averageMarks = totalMarks / Object.keys(studentMarks).length;
+
+//console.log("The average marks is " + averageMarks);
+
+//if (averageMarks >= 90) {
+ /// console.log("Grade: A");
+//} else if (averageMarks >= 80) {
+ // console.log("Grade: B");
+//} else if (averageMarks >= 70) {
+  //console.log("Grade: C");
+//} else if (averageMarks >= 60) {
+ // console.log("Grade: D");
+//} else {
+ // console.log("Grade: F");
+//}
+
+//question no 6
+
+//for (let i = 1; i <= 100; i++) {
+  //if (i % 3 === 0 && i % 5 === 0) {
+    //console.log("FizzBuzz");
+  //} else if (i % 3 === 0) {
+    //console.log("Fizz");
+ // } else if (i % 5 === 0) {
+    //console.log("Buzz");
+  //} else {
+  //  console.log(i);
+ // }
+//}
+
+// question no 7
+
+//for (let i = 1; i <= 5; i++) {
+  //let pattern = '';
+  //for (let j = 1; j <= i; j++) {
+   // pattern += '*';
+  //}
+ // console.log(pattern);
+//}
